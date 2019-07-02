f8e9b8f4902716832fb2c691bdea6b7f2213727f
Reno <reno.kim@nexstreaming.com>
2019-07-02 13:19:35 +0900

    괄호문제 푸는중

--- src/dfs/DfsSample.java
+++ src/dfs/DfsSample.java
@@ -1,47 +1,30 @@
 package dfs;
 
-import java.util.Stack;
+import java.util.*;
 
 public class DfsSample {
+    private static int N;
+    private static boolean[][] adj = new boolean[1001][1001];
+    private static boolean[] visited = new boolean[1001];
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        int M = sc.nextInt();
+        int V = sc.nextInt();
+        for(int i = 0; i < M; i++){
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adj[u][v] = adj[v][u] = true;
+        }
 
+        printDFS(V);
     }
 
-    //출처: https://mygumi.tistory.com/102 [마이구미의 HelloWorld]
-    //recursive
-    private static void dfs(int[][] a, boolean[] c, int v) {
-        int n = a.length - 1;
-        c[v] = true;
-        System.out.print(v + " ");
-        for (int i = 1; i <= n; i++) {
-            if (a[v][i] == 1 && !c[i]) {
-                dfs(a, c, i);
-            }
-        }
-    }
-
-    //stack
-    public static void dfs(int[][] a, boolean[] c, int v, boolean flag) {
-        Stack<Integer> stack = new Stack<>();
-        int n = a.length - 1;
-        stack.push(v);
-        c[v] = true;
-        System.out.print(v + " ");
-        while (!stack.isEmpty()) {
-            int vv = stack.peek();
-            flag = false;
-            for (int i = 1; i <= n; i++) {
-                if (a[vv][i] == 1 && !c[i]) {
-                    stack.push(i);
-                    System.out.print(i + " ");
-                    c[i] = true;
-                    flag = true;
-                    break;
-                }
-            }
-            if (!flag) {
-                stack.pop();
-            }
-        }
+    private static void printDFS(int cur){
+        System.out.print(cur + " ");
+        visited[cur] = true;
+        for(int i = 1; i <= N; i++)
+            if(adj[cur][i] && !visited[i]) printDFS(i);
     }
 }
--- src/dfs/DfsSolution3.java
+++ src/dfs/DfsSolution3.java
@@ -1,29 +1,68 @@
+/*
+4개의 기호 ‘(’, ‘)’, ‘[’, ‘]’를 이용해서 만들어지는 괄호열 중에서 올바른 괄호열이란 다음과 같이 정의된다.
+
+한 쌍의 괄호로만 이루어진 ‘()’와 ‘[]’는 올바른 괄호열이다.
+만일 X가 올바른 괄호열이면 ‘(X)’이나 ‘[X]’도 모두 올바른 괄호열이 된다.
+X와 Y 모두 올바른 괄호열이라면 이들을 결합한 XY도 올바른 괄호열이 된다.
+예를 들어 ‘(()[[]])’나 ‘(())[][]’ 는 올바른 괄호열이지만 ‘([)]’ 나 ‘(()()[]’ 은 모두 올바른 괄호열이 아니다.
+우리는 어떤 올바른 괄호열 X에 대하여 그 괄호열의 값(괄호값)을 아래와 같이 정의하고 값(X)로 표시한다.
+
+‘()’ 인 괄호열의 값은 2이다.
+‘[]’ 인 괄호열의 값은 3이다.
+‘(X)’ 의 괄호값은 2×값(X) 으로 계산된다.
+‘[X]’ 의 괄호값은 3×값(X) 으로 계산된다.
+올바른 괄호열 X와 Y가 결합된 XY의 괄호값은 값(XY)= 값(X)+값(Y) 로 계산된다.
+예를 들어 ‘(()[[]])([])’ 의 괄호값을 구해보자.  ‘()[[]]’ 의 괄호값이 2 + 3×3=11 이므로  ‘(()[[ ]])’의 괄호값은 2×11=22 이다.
+그리고  ‘([])’의 값은 2×3=6 이므로 전체 괄호열의 값은 22 + 6 = 28 이다.
+
+여러분이 풀어야 할 문제는 주어진 괄호열을 읽고 그 괄호값을 앞에서 정의한대로 계산하여 출력하는 것이다.
+
+input
+(()[[]])([])
+
+output
+28
+*/
 package dfs;
 
 import java.util.Scanner;
 
 public class DfsSolution3 {
+    private static String bracketString;
+    private static boolean[] visitedString;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String insert = scanner.nextLine();
+        String bracketString = scanner.nextLine();
+        visitedString = new boolean[bracketString.length()];
 
-        int result = calculateBracket(insert);
+        System.out.println(calculateBracket(0));
     }
 
-    private static int calculateBracket(String insert) {
-        //닫혀있는 괄호가 있나?
-        for (int i = 0; i < insert.length(); i++) {
+    private static int calculateBracket(int start) {
+        visitedString[start] = true;
 
+        String remain = bracketString.substring(start, start + 2);
 
-            if (insert.charAt(0) == '(' && insert.charAt(i) == ')') {//첫번째 글자와 닫힌 글자가 있다면
-                return 2 * calculateBracket(insert.substring(0, i));
-            } else if (insert.charAt(0) == '[' && insert.charAt(i) == ']') {
-                return 3 * calculateBracket(insert.substring(0,i));
+        if (remain.equals("()")){
+            return 2;
+        }
+        else if (remain.equals("[]")){
+            return 3;
+        }
+
+        int sum = 0;
+
+        for (int i = start; i < bracketString.length(); i++) {
+            if(!visitedString[i])
+
+            if (bracketString.charAt(start) == '(' && (!visitedString[i] && bracketString.charAt(i) == ')')) {
+                sum += 2 * calculateBracket(i);
+            } else if (!visitedString[i] && bracketString.charAt(i) == '[') {
+                sum += 3 * innerBracket;
             }
         }
-        //없으면 올바르지 못한 괄호열
 
-
-        return 0;
+        return sum;
     }
 }

45e2a3323eea4855d979a792ee91c8a28c41606e
reno-kim-km <reno.kim@kinemaster.com>
2020-01-20 13:11:06 +0900

    Between two sets (hacker rank)

---
+++ src/hackerrank/BetweenTwoSets.java
@@ -0,0 +1,82 @@
+package hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class BetweenTwoSets {
+
+    /*
+     * Complete the 'getTotalX' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static int getTotalX(List<Integer> a, List<Integer> b) {
+        // Write your code here
+        // array a 가장 작은수
+        // array b 가장 작은수
+        List<Integer> result = new ArrayList();
+        b.sort(null);
+        // loop aArr
+        for(int valueA : a) {
+            // Min(b) % a = 0 -> int[] resultArr add
+            if(b.get(0) % valueA == 0)
+                result.add(valueA);
+        }
+        // loop bArr
+        for (int valueB : b) {
+            for (int valueR : result) {
+                // b % resultArr[i] != 0 remove
+                if(valueB % valueR != 0){
+                    result.remove(valueR);
+                }
+            }
+
+        }
+        // return resultArr
+        return result.size();
+    }
+
+}
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> brr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int total = BetweenTwoSets.getTotalX(arr, brr);
+
+        bufferedWriter.write(String.valueOf(total));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+

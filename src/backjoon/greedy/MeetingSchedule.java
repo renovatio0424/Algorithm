d7049a4b401e01d0615ef7abf828d9bcb8b117e0
Reno <reno.kim@kinemaster.com>
2021-10-08 21:08:52 +0900

    회의실 배 [그리디] 풀이중2 - 메모리 초과
    출처 : 백준
    링크[1] : https://www.acmicpc.net/problem/1931
    링크[2] :

--- src/backjoon/greedy/MeetingSchedule.java
+++ src/backjoon/greedy/MeetingSchedule.java
@@ -1,13 +1,14 @@
 package backjoon.greedy;
 
-import java.util.*;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class MeetingSchedule {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        CopyOnWriteArrayList<int[]> scheduleArr = new CopyOnWriteArrayList<>();
+        ArrayList<int[]> scheduleArr = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             int startTime = scanner.nextInt();
@@ -23,16 +24,16 @@ public class MeetingSchedule {
         System.out.println(result);
     }
 
-    private static int getMaximumSchedule(CopyOnWriteArrayList<int[]> originalScheduleArr) {
-        CopyOnWriteArrayList<int[]> scheduleList = new CopyOnWriteArrayList<>();
+    private static int getMaximumSchedule(ArrayList<int[]> originalScheduleList) {
+        ArrayList<int[]> scheduleList = new ArrayList<>();
 
         //회의 시간 짧은 순으로
-        originalScheduleArr.sort(new Comparator<int[]>() {
+        originalScheduleList.sort(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                if (o1[1] - o1[0] < o2[1] - o2[0])
+                if (o1[1] < o2[1])
                     return -1;
-                else if (o1[1] - o1[0] == o2[1] - o2[0]) {
+                else if (o1[1] == o2[1]) {
                     if (o1[0] < o2[0])
                         return -1;
                     else
@@ -42,26 +43,13 @@ public class MeetingSchedule {
             }
         });
 
-        for (int[] schedule : originalScheduleArr) {
-            if (scheduleList.size() == 0 || scheduleList.size() == 1) {
+        for (int[] schedule : originalScheduleList) {
+            if (scheduleList.size() == 0)
                 scheduleList.add(schedule);
-                originalScheduleArr.remove(schedule);
-            }
 
-            for (int i = 0; i < scheduleList.size() - 1; i++) {
-                int[] scheduleElement1 = scheduleList.get(i);
-                int[] scheduleElement2 = scheduleList.get(i + 1);
-
-                if (i == 0 && scheduleElement1[0] >= schedule[1]) {
-                    scheduleList.add(i, schedule);
-                    originalScheduleArr.remove(schedule);
-                } else if (i == scheduleList.size() - 2 && scheduleElement2[1] <= schedule[0]){
-                    scheduleList.add(schedule);
-                    originalScheduleArr.remove(schedule);
-                } else if (scheduleElement1[1] <= schedule[0] && schedule[1] <= scheduleElement2[0]) {
-                    scheduleList.add(i + 1, schedule);
-                    originalScheduleArr.remove(schedule);
-                }
+            int[] lastSchedule = scheduleList.get(scheduleList.size() - 1);
+            if (lastSchedule[1] <= schedule[0]) {
+                scheduleList.add(schedule);
             }
         }
 

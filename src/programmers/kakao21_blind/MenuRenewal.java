6dcb2f9968dae29b47ebf728e6cc90b862889325
JungWon, Kim <renovatio0424@gmail.com>
2022-06-15 22:41:07 +0900

    [프로그래머스 - 카카오 블라인드 2021] 메뉴 리뉴얼
    문제 : https://programmers.co.kr/learn/courses/30/lessons/72411

--- src/programmers/kakao21_blind/MenuRenewal.java
+++ src/programmers/kakao21_blind/MenuRenewal.java
@@ -1,34 +1,102 @@
 package programmers.kakao21_blind;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class MenuRenewal {
     public static void main(String[] args) {
-
+        String[] orders = new String[]{
+                "ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"
+        };
+        int[] course = new int[]{2, 3, 4};
+        String[] result = solution(orders, course);
+        System.out.println(Arrays.toString(result));
     }
 
     public static String[] solution(String[] orders, int[] course) {
-        String[] answer = {};
+        ArrayList<String> result = new ArrayList<>();
+
+        //find total menu 26개
+        Character[] menuArr = getTotalMenuArr(orders);
 
         for (int courseCount : course) {
-            HashMap<String, Integer> menuMap = new HashMap<>();
-            getMenuList(orders, courseCount, menuMap);
-        }
+            ArrayList<String> courseList = findAllCourseCombination(menuArr, courseCount);
 
-        return answer;
-    }
-
-    private static void getMenuList(String[] orders, int courseCount, HashMap<String, Integer> menuMap) {
-        ArrayList<String> menuList = new ArrayList<>();
-
-        for (String order : orders) {
-            for (int i = 0; i < order.length(); i++) {
-                for (int j = i + 1; j < order.length(); j++) {
-
+            int maxContainCount = 0;
+            for (String courseMenu : courseList) {
+                int orderMember = countOrderMember(orders, courseMenu);
+                if (orderMember >= 2 && maxContainCount < orderMember) {
+                    maxContainCount = orderMember;
+                    result.removeIf(resultElement -> resultElement.length() == courseCount);
+                    result.add(courseMenu);
+                } else if (orderMember >= 2 && maxContainCount == orderMember) {
+                    result.add(courseMenu);
                 }
             }
         }
 
+        Collections.sort(result);
+        String[] resultArr = new String[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            resultArr[i] = result.get(i);
+        }
+
+        return resultArr;
+    }
+
+    private static ArrayList<String> findAllCourseCombination(Character[] menuArr, int courseCount) {
+        boolean[] visited = new boolean[menuArr.length];
+        ArrayList<String> courseList = new ArrayList<>();
+        combination(menuArr, visited, 0, menuArr.length, courseCount, courseList);
+        return courseList;
+    }
+
+    private static Character[] getTotalMenuArr(String[] orders) {
+        HashSet<Character> menuSet = new HashSet<>();
+        for (String order : orders) {
+            for (char menu : order.toCharArray()) {
+                menuSet.add(menu);
+            }
+        }
+
+        //courseList
+        Character[] menuArr = new Character[menuSet.size()];
+        menuSet.toArray(menuArr);
+        return menuArr;
+    }
+
+    private static int countOrderMember(String[] orders, String courseMenu) {
+        int orderMemberCount = 0;
+        for (String order: orders) {
+            int containedCourseCount = 0;
+            for (char course: courseMenu.toCharArray()) {
+                if (order.indexOf(course) != -1) {
+                    containedCourseCount++;
+                }
+            }
+
+            if (containedCourseCount == courseMenu.length()) orderMemberCount++;
+        }
+
+        return orderMemberCount;
+    }
+
+    static void combination(Character[] menuArr, boolean[] visited, int start, int n, int r, ArrayList<String> courseList) {
+        if(r == 0) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++) {
+                if (visited[i]) sb.append(menuArr[i]);
+            }
+            String course = sb.toString();
+            if (course.isEmpty() || course.isBlank()) return;
+
+            courseList.add(sb.toString());
+            return;
+        }
+
+        for(int i=start; i<n; i++) {
+            visited[i] = true;
+            combination(menuArr, visited, i + 1, n, r - 1, courseList);
+            visited[i] = false;
+        }
     }
 }
